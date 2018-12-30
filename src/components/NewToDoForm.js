0c24fada18fa9505f1f261d0c8e596466cc1e9de
mrabenda92 <exo92mr@gmail.com>
2018-12-30 16:33:44 +0100

    1.3

--- src/components/NewToDoForm.js
+++ src/components/NewToDoForm.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Button = styled.button`
+    background: #232632;
+    color: #00a7fa;
+    width: 80px;
+    height: 32px;
+    font-size: 1.7em;
+    border: 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const TextInput = styled.input`
+  padding: 5px;
+  font-size: .7em;
+  background: #232632;
+  color: #d3d4d6;
+  width: 100%;
+  margin-right: 7px;
+  border: 0px;
+  -webkit-appearance: none;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid #343744;
+  background: #232632;
+  border-radius: 10px;
+  padding: 5px;
+`
 
 const NewToDoForm = ({onChange, draft, onSubmit}) => (
-    <div>
-      <input type="text" onChange={onChange} value={draft}></input>
-      <button onClick={onSubmit}>Add</button>
-    </div>
+    <Container>
+      <TextInput type="text" onChange={onChange} value={draft}></TextInput>
+      <Button onClick={onSubmit}>Add</Button>
+    </Container>
   )
 export default NewToDoForm  
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import ToDoItem from './ToDoItem.js'
 import NewToDoForm from './NewToDoForm.js'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background: #2b2e39;
+  margin: 0 auto;
+  width: 80%;
+  max-width: 600px;
+  padding: 14px;
+  border-radius: 14px;
+  margin-top: 14px;
+`
+
+const Header = styled.h1`
+  color: white;
+`
+
+const DestroyButton = styled.button`
+  border-radius: 10px;
+  background: red;
+  padding: 5px;
+  color: white;
+  margin-bottom: 10px;
+`
 
 class ToDoList extends Component {
+    componentDidMount = () => {
+      fetch('http://localhost:5000/todo_items')
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }
+
     static defaultProps = {
-      tasks: [
-        {text: `Buy bread`},
-        {done: false, text: `Buy cheese`}
-      ]
+      tasks: [], 
+      title: "To do List"
     }
 
     state = {
@@ -22,24 +49,31 @@ class ToDoList extends Component {
     addToDo = () => {
       const { tasks, draft } = this.state;
       const tasksList = tasks;
-      tasksList.push({text: draft, done: false})
-      this.setState({tasks: tasksList, draft: ''})
-      // this.setState({tasks: [...tasksList, {text: draft}], draft: ""})
+
+      if (draft !=="") {
+        tasksList.push({text: draft, done: false})
+        this.setState({tasks: tasksList, draft: ''})
+        // this.setState({tasks: [...tasksList, {text: draft}], draft: ""})
+    }}
+
+    removeAll = () => {
+      this.setState({tasks: []})
     }
   
     render() {
-      const { tasks } = this.props;
-      const { draft } = this.state;
+      const {title} = this.props
+      const { tasks, draft } = this.state;
       
       return (
-        <div>
-          <h1>To Do List</h1>
+        <Container>
+          <Header>{title}</Header>
+          <DestroyButton onClick={this.removeAll}>Remove all</DestroyButton>
           <p>{tasks.map(work => <ToDoItem text={work.text} done={work.done} />)}</p>
           <NewToDoForm 
             onSubmit={this.addToDo}
             onChange={this.updateDraft}
             draft={draft} />
-        </div>
+        </ Container>
         
       )
     }
