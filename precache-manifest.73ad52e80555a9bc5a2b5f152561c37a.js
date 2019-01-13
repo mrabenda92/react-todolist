0a4b03514f66928dc3e93ef5c5fb60ed24792405
mrabenda92 <exo92mr@gmail.com>
2019-01-14 00:35:58 +0100

    Updates

---
+++ precache-manifest.73ad52e80555a9bc5a2b5f152561c37a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15e18c78eb78a1dce817",
+    "url": "/react-todolist/static/js/runtime~main.15e18c78.js"
+  },
+  {
+    "revision": "858ef8cec7a40b0e2084",
+    "url": "/react-todolist/static/js/main.858ef8ce.chunk.js"
+  },
+  {
+    "revision": "5e1ee69fe3d2a6adb77c",
+    "url": "/react-todolist/static/js/1.5e1ee69f.chunk.js"
+  },
+  {
+    "revision": "858ef8cec7a40b0e2084",
+    "url": "/react-todolist/static/css/main.a1367427.chunk.css"
+  },
+  {
+    "revision": "8876232ba5b13434a9353338a950a9ce",
+    "url": "/react-todolist/index.html"
+  }
+];
